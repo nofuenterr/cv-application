5d6cb2082ed9a7174716296b0f2bc0f63ac5f159
RR <rrn091301@gmail.com>
2025-11-15 11:50:47 +0800

    feat: import new css files to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+/* import './index.css' */
+import './modern-normalize.css'
+import './reset.css'
+import './styles.css'
 import App from './components/App.jsx'
 
 createRoot(document.getElementById('root')).render(
